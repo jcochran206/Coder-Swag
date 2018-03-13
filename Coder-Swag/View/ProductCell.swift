c605c74bccdb6b42201145caf862a5185447a139
jcochran206 <jonathan.o.cochran@gmail.com>
2018-03-14 00:03:05 +0800

    added custom collection views

---
+++ Coder-Swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  Coder-Swag
+//
+//  Created by Jonathan Cochran on 3/13/18.
+//  Copyright © 2018 Jonathan Cochran. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
