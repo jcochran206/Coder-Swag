1a2fab0bfbc1817756670f43a26e23115aa28f8b
jcochran206 <jonathan.o.cochran@gmail.com>
2018-03-16 16:46:05 +0800

    updated custom cell to provide items for user to view

--- Coder-Swag/Controller/CategoriesVC.swift
+++ Coder-Swag/Controller/CategoriesVC.swift
@@ -32,5 +32,21 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
             return CategoryCell()
         }
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let ProductVC = segue.destination as? ProductVC {
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
+            
+            assert(sender as? Category != nil)
+            ProductVC.initProducts(category: sender as! Category)
+        }
+    }
 }
 
--- Coder-Swag/Services/dataService.swift
+++ Coder-Swag/Services/dataService.swift
@@ -16,8 +16,67 @@ class DataService {
         Category(title: "HATS", imageName: "hats.png"),
         Category(title: "DIGITAL", imageName: "digital.png")
     ]
+    
+    private let hats = [
+        Product(title: "DevSlopes graphic beanie", price: "$18", imageName: "hat01.png"),
+        Product(title: "DevSlopes Logo Hat Black", price: "$25", imageName: "hat02.png"),
+        Product(title: "DevSlopes Logo Hat white", price: "$25", imageName: "hat03.png"),
+        Product(title: "DevSlopes Logo Snapback", price: "$30", imageName: "hat04.png"),
+    
+    ]
+    
+    private let hoodies = [
+        Product(title: "DevSlopes Hoodie Grey", price: "$40", imageName: "hoodie01.png"),
+        Product(title: "DevSlopes Hoodie red", price: "$40", imageName: "hoodie02.png"),
+        Product(title: "DevSlopes Hoodie Grey", price: "$40", imageName: "hoodie03.png"),
+        Product(title: "DevSlopes Hoodie Black", price: "$40", imageName: "hoodie04.png")
+    ]
+    
+    private let shirts = [
+        Product(title: "DevSlopes Shirt Black", price: "15", imageName: "shirt01.png"),
+        Product(title: "DevSlopes Badge shirt light grey", price: "15", imageName: "shirt02.png"),
+        Product(title: "DevSlopes Shirt Black", price: "15", imageName: "shirt03.png"),
+        Product(title: "DevSlopes Shirt Black", price: "15", imageName: "shirt04.png"),
+        Product(title: "DevSlopes Shirt Black", price: "15", imageName: "shirt05.png")
+    ]
+    
+    private let digitalGoods  = [Product]()
+    
     func getCategories() -> [Category] {
         
         return categories
     }
+    
+    func getProducts(forCategoryTitle title: String) -> [Product] {
+        switch title {
+        case "SHIRTS":
+            return getShirts()
+        case "HATS":
+             return getHats()
+        case "HOODIES":
+            return getHoodies()
+        case "DIGITAL":
+            return getDigital()
+        default:
+            return getShirts()
+        }
+    }
+    func getHats() -> [Product] {
+        return hats
+    }
+    func getShirts() -> [Product]{
+        return shirts
+    }
+    
+    func getHoodies() -> [Product] {
+        return hoodies
+    }
+    
+    func getDigital() -> [Product]{
+        return digitalGoods
+    }
+    
+    
+    
+    
 }
